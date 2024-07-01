e88b0c60d76a2f0bd96bcd9778dac940b832f64b
vrindger <vrindger@gmail.com>
2024-07-01 16:43:05 -0600

    save accepted solution

--- merge_sorted.py
+++ merge_sorted.py
@@ -1,30 +1,29 @@
-import copy
+# https://leetcode.com/problems/merge-sorted-array/?envType=study-plan-v2&envId=top-interview-150
+# Merge sorted arrays
+
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = j = 0
-        merged_nums = []
-        count = 0
-        for count in range(m+n+1):
-            print(i, j, count, merged_nums)
-            if i < m:
-                if j < n:
-                    if nums1[i] <= nums2[j]:
-                        merged_nums.append(nums1[i])
-                        i+=1
+        i = m - 1
+        j = n - 1
+        last_elem = m + n - 1
+
+        while i > 0 or j >= 0:
+            if i >= 0:
+                if j >= 0:
+                    if nums1[i] < nums2[j]:
+                        nums1[last_elem] = nums2[j]
+                        j -= 1
                     else:
-                        merged_nums.append(nums2[j])
-                        j+=1
+                        nums1[last_elem] = nums1[i]
+                        i -= 1
                 else:
-                    merged_nums.append(nums1[i])
-                    i+=1
+                    nums1[last_elem] = nums1[i]
+                    i -= 1
             else:
-                if j < n:
-                    merged_nums.append(nums2[j])
-                    j+=1
-        nums1=[]
-        for i in merged_nums:
-            nums1.append(i)
-        print(nums1)
+                nums1[last_elem] = nums2[j]
+                j -= 1
+            last_elem -=1
+
